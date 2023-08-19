ca37325b114ebd9fe2cc0cd335c7791f6311c748
HanhTran1911 <tranhanh191103@gmail.com>
2023-08-19 15:06:09 +0700

    insert infor seeder

--- database/seeders/GraveLocationsSeeder.php
+++ database/seeders/GraveLocationsSeeder.php
@@ -35,6 +35,97 @@ class GraveLocationsSeeder extends Seeder
                 'latitude'=>'-34.12146681321436',
                 'longitude'=>'22.99218535423279'
             ],
+            [
+                'grave_number' => '2',
+                'grave_row' => '1',
+                'grave_lot' => '1',
+                'grave_area' => 'C',
+                'other_info' => 'Thông tin mẫu cho lô mộ 3',
+                'grave_type' => 'Loại A',
+                'has_remains' => true,
+                'latitude'=>'28.264517196510898',
+                'longitude'=>'48.86718750000001'
+            ],
+            
+            [
+                'grave_number' => '2',
+                'grave_row' => '4',
+                'grave_lot' => '1',
+                'grave_area' => 'C',
+                'other_info' => 'Thông tin mẫu cho lô mộ 4',
+                'grave_type' => 'Loại A',
+                'has_remains' => true,
+                'latitude'=>'62.58959459710922',
+                'longitude'=>'41.13281250000001'
+            ],
+            
+            [
+                'grave_number' => '4',
+                'grave_row' => '4',
+                'grave_lot' => '3',
+                'grave_area' => 'D',
+                'other_info' => 'Thông tin mẫu cho lô mộ 5',
+                'grave_type' => 'Loại A',
+                'has_remains' => true,
+                'latitude'=>'71.35588770660311',
+                'longitude'=>'18.017578125000004'
+            ],
+            [
+                'grave_number' => '3',
+                'grave_row' => '1',
+                'grave_lot' => '1',
+                'grave_area' => 'A',
+                'other_info' => 'Thông tin mẫu cho lô mộ 6',
+                'grave_type' => 'Loại A',
+                'has_remains' => true,
+                'latitude'=>'73.15273416947426',
+                'longitude'=>'-77.84912109375'
+            ],
+            [
+                'grave_number' => '6',
+                'grave_row' => '4',
+                'grave_lot' => '2',
+                'grave_area' => 'B',
+                'other_info' => 'Thông tin mẫu cho lô mộ 7',
+                'grave_type' => 'Loại B',
+                'has_remains' => true,
+                'latitude'=>'-18.025171403136834',
+                'longitude'=>'-64.16015625000001'
+            ],
+            [
+                'grave_number' => '3',
+                'grave_row' => '2',
+                'grave_lot' => '2',
+                'grave_area' => 'B',
+                'other_info' => 'Thông tin mẫu cho lô mộ 8',
+                'grave_type' => 'Loại A',
+                'has_remains' => true,
+                'latitude'=>'-31.404443033990923',
+                'longitude'=>'-42.01171875000001'
+            ],
+            [
+                'grave_number' => '9',
+                'grave_row' => '2',
+                'grave_lot' => '5',
+                'grave_area' => 'C',
+                'other_info' => 'Thông tin mẫu cho lô mộ 9',
+                'grave_type' => 'Loại A',
+                'has_remains' => true,
+                'latitude'=>'46.54119556205806',
+                'longitude'=>'24.433593750000004'
+            ],
+            [
+                'grave_number' => '7',
+                'grave_row' => '2',
+                'grave_lot' => '3',
+                'grave_area' => 'C',
+                'other_info' => 'Thông tin mẫu cho lô mộ 10',
+                'grave_type' => 'Loại A',
+                'has_remains' => true,
+                'latitude'=>'56.16195176619384',
+                'longitude'=>'49.74609375000001'
+            ],
+            
         ];
 
         DB::table('grave_locations')->insert($graveLocations);
--- database/seeders/MartyrsSeeder.php
+++ database/seeders/MartyrsSeeder.php
@@ -34,6 +34,86 @@ class MartyrsSeeder extends Seeder
                 'grave_location_id' => 2, 
                 'hometowns'=>"Đồng Tháp"
             ],
+            [
+                'full_name' => 'Hồ Văn C',
+                'date_of_birth' => '1987-05-10',
+                'rank' => 'Binh nhì',
+                'unit' => 'Đơn vị C',
+                'position' => 'Chức vụ B',
+                'date_of_sacrifice' => '2000-10-3',
+                'grave_location_id' => 3, 
+                'hometowns'=>"Ninh Thuận"
+            ],
+            [
+                'full_name' => 'Nguyễn Thị D',
+                'date_of_birth' => '1950-04-19',
+                'rank' => 'Binh nhì',
+                'unit' => 'Đơn vị D',
+                'position' => 'Chức vụ A',
+                'date_of_sacrifice' => '1998-01-23',
+                'grave_location_id' => 4, 
+                'hometowns'=>"Bình Định"
+            ],
+            [
+                'full_name' => 'Trần Văn A2',
+                'date_of_birth' => '1960-04-19',
+                'rank' => 'Binh nhì',
+                'unit' => 'Đơn vị B',
+                'position' => 'Chức vụ A',
+                'date_of_sacrifice' => '1995-01-23',
+                'grave_location_id' => 5, 
+                'hometowns'=>"Bình Định"
+            ],
+            [
+                'full_name' => 'Trần Văn C',
+                'date_of_birth' => '1958-06-19',
+                'rank' => 'Binh nhì',
+                'unit' => 'Đơn vị B',
+                'position' => 'Chức vụ A',
+                'date_of_sacrifice' => '1990-06-23',
+                'grave_location_id' => 6, 
+                'hometowns'=>"Huế"
+            ],
+            [
+                'full_name' => 'Cao Thị H',
+                'date_of_birth' => '1960-06-19',
+                'rank' => 'Binh nhì',
+                'unit' => 'Đơn vị B',
+                'position' => 'Chức vụ A',
+                'date_of_sacrifice' => '1992-06-23',
+                'grave_location_id' => 7, 
+                'hometowns'=>"Huế"
+            ],
+            [
+                'full_name' => 'Hoàng Văn C',
+                'date_of_birth' => '1955-04-19',
+                'rank' => 'Binh nhì',
+                'unit' => 'Đơn vị B',
+                'position' => 'Chức vụ A',
+                'date_of_sacrifice' => '1990-05-12',
+                'grave_location_id' => 8, 
+                'hometowns'=>"Đà Nẵng"
+            ],
+            [
+                'full_name' => 'Hoàng Văn D',
+                'date_of_birth' => '1955-04-02',
+                'rank' => 'Binh nhất',
+                'unit' => 'Đơn vị C',
+                'position' => 'Chức vụ A',
+                'date_of_sacrifice' => '1994-05-12',
+                'grave_location_id' => 9, 
+                'hometowns'=>"Đà Nẵng"
+            ],
+            [
+                'full_name' => 'Đinh Văn D',
+                'date_of_birth' => '1965-04-02',
+                'rank' => 'Binh nhất',
+                'unit' => 'Đơn vị C',
+                'position' => 'Chức vụ A',
+                'date_of_sacrifice' => '1995-05-12',
+                'grave_location_id' => 10, 
+                'hometowns'=>"Long An"
+            ],
         ];
 
         // Thêm dữ liệu vào bảng "Martyrs"
